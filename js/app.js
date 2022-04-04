012532f0f44f9ebaecf9d291f4a799e8fe5ea279
Zorin Mikhail <rivixxx@gmail.com>
2022-04-04 22:31:18 +0300

    fix (add media quires)

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 'use strict'
+
+// бургер меню =================================================================
+
+const burgerMenu = document.querySelector('.burger__menu');
+const menuBody = document.querySelector('.secondRow_navigation');
+if (burgerMenu) {
+    burgerMenu.addEventListener('click', function(e) {
+        document.body.classList.toggle('_lock');
+        burgerMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
